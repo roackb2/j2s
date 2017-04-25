43e4eae4753c6a313d0b29f92bbbea987981ec6a
Jay Liang <roackb2@gmail.com>
2017-04-25 16:48:12 +0800

    throw resource not found when updating objects but none exists in database

--- src/index.js
+++ src/index.js
@@ -152,6 +152,9 @@ async function createInstances(bookshelf, ctx, data, controller, path, opts, for
 
 async function updateInstances(bookshelf, ctx, query, data, controller, path, opts, forbids) {
     let instances = await core.query(bookshelf, opts.model, query).fetch({require: true});
+    if (!instances || instances.length == 0) {
+        throw errors.ErrResourceNotFound;
+    }
     let check = await core.check(ctx, opts.identity.U, opts.admin.U, instances, opts.access.U);
     if (!check) {
         throw errors.ErrOperationNotAuthorized;
